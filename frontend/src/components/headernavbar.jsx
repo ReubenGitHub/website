78a814bb1a46688ac775cffd4f2c3000b075c0f8
reuben <reubenowenwilliams@outlook.com>
2026-07-11 20:19:52 +0000

    Add Projects dropdown to navbar, fix dropdown styling and hover

--- frontend/src/components/headernavbar.jsx
+++ frontend/src/components/headernavbar.jsx
@@ -16,8 +16,13 @@ export function NavBar() {
             <div class="logo"> <img class="img" src={logo_white} alt="Logo"></img> </div>
             <div class="nav-links">
                 <a href="/" class={active[0]}>Home</a>
-                <a href="/machinelearner" class={active[1]}>Machine Learner</a>
-                <a href="/pathfinder" class={active[2]}>Path-finder</a>
+                    <div class="dropdown">
+                        <span class="dropdown-btn">Projects ▾</span>
+                        <div class="dropdown-content">
+                            <a href="/machinelearner" class={active[1]}>Machine Learner</a>
+                            <a href="/pathfinder" class={active[2]}>Path-finder</a>
+                        </div>
+                    </div>
             </div>
             <a href="https://www.linkedin.com/in/reuben-owen-williams-53609a173/" target="_blank" class="right">LinkedIn</a>
             </div>
